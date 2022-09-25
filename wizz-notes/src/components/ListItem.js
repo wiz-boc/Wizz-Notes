4427a964601f1e9742a0442ce1d9b8cbef2f48a6
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-09-24 21:05:29 -0500

    Complete notes app

--- wizz-notes/src/components/ListItem.js
+++ wizz-notes/src/components/ListItem.js
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
 import React from "react";
 
+let getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+let getTitle = (note) => {
+  const title = note.body.split("\n")[0];
+  //if(title.length > 45)
+  return title.slice(0, 45);
+};
+
+let getContent = (note) => {
+  let title = getTitle(note);
+  let content = note.body.replaceAll("\n", "");
+  content.replaceAll(title, "");
+  if (content.length > 45) {
+    content = content.slice(0, 45) + "...";
+  }
+  return content;
+};
+
 const ListItem = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3>{getTitle(note)}</h3>
+        <p>
+          <span>{getTime(note)}</span>
+          {getContent(note)}
+        </p>
       </div>
     </Link>
   );
--- wizz-notes/src/pages/NotePage.js
+++ wizz-notes/src/pages/NotePage.js
@@ -1,22 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
-import { useParams, Link } from "react-router-dom";
-import notes from "../assets/data";
+import { useParams, Link, useNavigate } from "react-router-dom";
+//import notes from "../assets/data";
 
 const NotePage = () => {
   let { id } = useParams();
-  let note = notes.find((note) => note.id == id);
+  //let note = notes.find((note) => note.id == id);
+  let [note, setNote] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getNote();
+  }, [id]);
+
+  let getNote = async () => {
+    if (id === "new") return;
+    let response = await fetch(`http://127.0.0.1:5000/notes/${id}`);
+    let data = await response.json();
+    setNote(data);
+  };
+
+  let updateNote = async () => {
+    await fetch(`http://127.0.0.1:5000/notes/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+    navigate("/");
+  };
+
+  let deleteNote = async () => {
+    await fetch(`http://127.0.0.1:5000/notes/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    navigate("/");
+  };
+
+  let createNote = async () => {
+    await fetch(`http://127.0.0.1:5000/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+    navigate("/");
+  };
+
+  let handleSubmit = () => {
+    if (id != "new" && !note.body) {
+      deleteNote();
+    } else if (id !== "new") {
+      updateNote();
+    } else if (id === "new" && note !== null) {
+      createNote();
+    }
+  };
 
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+        {id !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={createNote}>Done</button>
+        )}
       </div>
-      <textarea value={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 };
